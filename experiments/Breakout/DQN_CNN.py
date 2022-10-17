975a6e0958c44bf1f51b401b6fb6f09d5b63596d
aberhu <aberhu@tencent.com>
2022-10-17 16:44:19 +0800

    note

--- experiments/Breakout/DQN_CNN.py
+++ experiments/Breakout/DQN_CNN.py
@@ -33,11 +33,15 @@ def process_frame(frame, shape=INPUT_SHAPE):
     Returns:
         The processed frame
     """
+    # (210, 160, 3)
     frame = frame.astype(np.uint8)  # cv2 requires np.uint8, other dtypes will not work
-
+    # (210, 160)
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # (160, 160), 不明白这里为什么要用34来裁剪图片
     frame = frame[34:34+160, :160]  # crop image
+    # (84, 84)
     frame = cv2.resize(frame, shape, interpolation=cv2.INTER_NEAREST)
+    # (84, 84, 1)
     frame = frame.reshape((*shape, 1))
 
     return frame
